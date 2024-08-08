16fcbfa348c399331aaa48a5da5bc541e59ebad8
Eric-2369 <admin@eri.cx>
2024-08-08 17:53:09 +0800

    init: initial java project for idea plugin

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.17.4"
+}
+
+group = "cx.eri.guttercoveragedisplay"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    version.set("2024.1")
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.11.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
