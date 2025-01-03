ec214653914495df5fa49fc8753cd9d0bd598c52
Eric-2369 <admin@eri.cx>
2025-01-03 17:20:00 +0800

    refactor: move plugin version, description, and change-notes to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,54 @@ dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
 
     intellijPlatform {
-        create("IC", "2024.1")
+        create("IC", "2024.2")
         bundledPlugins("Git4Idea")
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        id = "cx.eri.gutter-coverage-display"
+        name = "Gutter Coverage Display"
+        version = project.version.toString()
+        description = """
+            <p>A simple IntelliJ-based plugin to display and toggle coverage data in the gutter, specifically for the repository
+            <a href="https://github.com/techops-e2ecs/psa-sfdx">psa-sfdx</a>.</p>
+            <p>This plugin allows users to read external coverage JSON files and display the coverage data directly in the gutter of the code editor.</p>
+            <ul>
+                <li>Toggle coverage data visibility</li>
+                <li>Supports custom JSON coverage files</li>
+                <li>Seamless integration with Git</li>
+            </ul>
+            <p>For more information, please visit our <a href="https://github.com/Eric-2369/GutterCoverageDisplay">project page</a>.</p>
+            """.trimIndent()
+        changeNotes = """
+            <h2>Version 1.0.2</h2>
+            <ul>
+                <li>Updated dependencies to ensure compatibility with IntelliJ IDEA 2024.2.</li>
+                <li>Increased the minimum supported version to IntelliJ IDEA 2024.2.</li>
+            </ul>
+            <h2>Version 1.0.1</h2>
+            <ul>
+                <li>Updated plugin icon.</li>
+                <li>Improved plugin description.</li>
+            </ul>
+            <h2>Initial Release - Version 1.0.0</h2>
+            <ul>
+                <li>Initial release of Gutter Coverage Display plugin.</li>
+                <li>Added feature to read and display coverage data from external JSON files.</li>
+                <li>Implemented toggle functionality for coverage data visibility.</li>
+            </ul>
+            """.trimIndent()
+        vendor {
+            name = "Eric2369"
+            email = "admin@eri.cx"
+            url = "https://eri.cx"
+        }
+        ideaVersion {
+            sinceBuild = "242"
+            untilBuild = provider { null }
+        }
     }
 }
 
